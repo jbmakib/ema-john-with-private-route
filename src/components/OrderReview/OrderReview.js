0799b7ddf6d37f7fc07e5e9c3dfd0ecbed60e0ab
jbmakib <mdfoujolmobin@gmail.com>
2021-10-03 06:53:13 +0600

    cart added in review page

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,12 +1,17 @@
 import React from "react";
+import useCart from "../../hooks/useCart";
 import useProducts from "../../hooks/useProducts";
+import Cart from "../Cart/Cart";
 
 const OrderReview = () => {
-    const [products] = useProducts();
+    const [products, setProducts] = useProducts();
+    const [cart] = useCart(products);
     return (
-        <div>
-            <h1>{products.length}</h1>
-            <h2>This is Order Review</h2>
+        <div className="shop-container">
+            <div className="product-container">This is order review</div>
+            <div>
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -10,7 +10,7 @@ const useProducts = () => {
     }, []);
 
     // return necessary things
-    return [products];
+    return [products, setProducts];
 };
 
 export default useProducts;
