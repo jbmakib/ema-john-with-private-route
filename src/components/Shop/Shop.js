1680ffb87652e16fe53e93ee2062262d4df0d3f7
jbmakib <mdfoujolmobin@gmail.com>
2021-10-03 21:02:08 +0600

    cart bug fixed

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -39,7 +39,16 @@ const Shop = () => {
     }, [products]);
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        const exist = cart.find((pd) => pd.key === product.key);
+        let newCart = [...cart, product];
+        if (exist) {
+            const rest = cart.filter((pd) => pd.key !== product.key);
+            exist.quantity += 1;
+            newCart = [...rest, product];
+        } else {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
         setCart(newCart);
         // save to local storage (for now)
         addToDb(product.key);
